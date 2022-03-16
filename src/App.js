515cf3a0ad6df8f9132eca701484609bee8517f2
safa abid <safaabid.12@gmail.com>
2022-03-16 15:45:48 +0100

    créer la page Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import {Routes ,Route} from "react-router-dom"
+import Home from './components/Pages/Home/Home";
+
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element="/Home" />
+        <Route path="/" element={<Home/>} />
       </Routes>
     </div>
   );
