dc359c9b08435ca19689e48e34538f047943ee1c
safa abid <safaabid.12@gmail.com>
2022-04-20 02:05:31 +0200

    créeer card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,14 @@
+
+import styles from "./Card.module.scss";
+import React from 'react';
+//react icons
+
+const Card = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Card;
