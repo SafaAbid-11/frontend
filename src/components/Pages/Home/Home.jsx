03756152b06eea7f429089057a5eca4f23680750
safa abid <safaabid.12@gmail.com>
2022-04-21 04:16:40 +0200

    ajout de la section 5

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Section1 from './Sections/Section1';
 import Section2 from './Sections/Section2';
 import Section3 from './Sections/Section3';
 import Section4 from './Sections/Section4';
+import Section5 from './Sections/Section5';
  const Home = () => {
   return (
     <main>
@@ -11,6 +12,7 @@ import Section4 from './Sections/Section4';
       <Section2 />
       <Section3 />
       <Section4 />
+      <Section5 />
     </main>
   )
 };
